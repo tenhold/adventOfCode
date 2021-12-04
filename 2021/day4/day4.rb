9ab2504b96bece4c9af5a356a2ec1ab46c4c9364
tenhold <tenhold@gmail.com>
2021-12-04 12:42:20 -0600

    (completed) pt1

--- 2021/day4/day4.rb
+++ 2021/day4/day4.rb
@@ -1,12 +1,12 @@
 require 'pry'
 require 'matrix'
 
-inputs = File.read('test_inputs.txt').split(/\n\n/)
+inputs = File.read('inputs.txt').split(/\n\n/)
 
 # split each num into an array
-nums = inputs[0].split(',')
+nums = inputs[0].split(',').map { |num| num.to_i }
 # split each board in to a matrix
-boards = inputs[1..-1].map { |b| b.split(/\n/).map { |r| r.split(' ').map { |cell| { num: cell, seen: false } } } }
+boards = inputs[1..-1].map { |b| b.split(/\n/).map { |r| r.split(' ').map { |cell| { num: cell.to_i, seen: false } } } }
 
 # convert the array of arrays into a ruby Matrix
 # loop over the Matrix and check each element to see if the number is the same as the picked number
@@ -20,6 +20,8 @@ board_2 = Matrix[*boards[1]]
 board_3 = Matrix[*boards[2]]
 
 def bingo(board, nums)
+  column = nil
+  row = nil
   nums.each.with_index do |picked_num, count|
     board.each_with_index do |cell, r, c|
       if cell[:num] == picked_num
@@ -27,12 +29,30 @@ def bingo(board, nums)
       end
 
       if board.column(c).all? { |ele| ele[:seen] == true } || board.row(r).all? { |ele| ele[:seen] == true }
-        return count #"FOUND!!! #{board[r,c]}"
+        unmarked_nums = board.inject(0) do |sum, hash|
+          sum += hash[:num] if hash[:seen] == false
+          sum
+        end
+        return {
+          time: count,
+          unmarked_nums: unmarked_nums,
+          picked_num: picked_num
+        }
       end
+      
     end
   end
 end
 
-puts bingo(board_1, nums)
-puts bingo(board_2, nums)
-puts bingo(board_3, nums)
+winner = nil
+
+boards.each do |board|
+  m_board = Matrix[*board]
+  game = bingo(m_board, nums)
+  winner = game if winner.nil?
+  if game[:time] < winner[:time]
+    winner =  game
+  end
+end
+
+puts winner[:unmarked_nums] * winner[:picked_num]
