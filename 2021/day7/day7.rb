238c2f16d92fecf0066f54050befa4c30281a338
tenhold <tenhold@gmail.com>
2021-12-07 18:57:38 -0600

    (add) pt 1 and 2

---
+++ 2021/day7/day7.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+time = Time.now.utc
+
+TEST_INPUTS = [16,1,2,0,4,2,7,1,2,14]
+
+inputs = File.read('inputs.txt').split(',').map(&:to_i)
+
+def get_moves(inputs)
+  total_posititions = inputs.tally
+
+  fuels = []
+  
+  (inputs.min..inputs.max).each do |num|
+    fuels << inputs.inject(0) do |sum, crab|
+      diff = (crab - num).abs
+      sub_sum = (1..diff).inject(0, :+)
+      sum += sub_sum
+      sum
+    end
+
+  end
+  # return inputs.inject(0) do |sum, crab|
+  #   least_moves < crab ? sum += crab - least_moves : sum += least_moves - crab
+  #   sum
+  return fuels.min
+end
+
+puts get_moves(inputs)
+puts Time.now.utc - time
